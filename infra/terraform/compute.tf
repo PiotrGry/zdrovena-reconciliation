0b74d4790d374e85e07e84c2c61881a62a272ef4
PiotrGry <piotrgryzlo@gmail.com>
2026-05-19 14:35:35 +0200

    fix(terraform): terraform fmt compute.tf

--- infra/terraform/compute.tf
+++ infra/terraform/compute.tf
@@ -5,19 +5,19 @@
 module "api_prod" {
   source = "./modules/container_app"
 
-  name                         = "${var.prefix}-api-prod"
-  environment                  = "prod"
-  resource_group_name          = azurerm_resource_group.rg.name
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  acr_login_server             = azurerm_container_registry.acr.login_server
-  acr_id                       = azurerm_container_registry.acr.id
-  storage_account_url          = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net"
-  storage_container_name       = azurerm_storage_container.files.name
-  storage_container_id         = azurerm_storage_container.files.id
-  storage_account_id           = azurerm_storage_account.storage.id
-  key_vault_id                 = azurerm_key_vault.kv.id
-  key_vault_url                = azurerm_key_vault.kv.vault_uri
-  azure_tenant_id              = var.azure_tenant_id
+  name                                  = "${var.prefix}-api-prod"
+  environment                           = "prod"
+  resource_group_name                   = azurerm_resource_group.rg.name
+  container_app_environment_id          = azurerm_container_app_environment.env.id
+  acr_login_server                      = azurerm_container_registry.acr.login_server
+  acr_id                                = azurerm_container_registry.acr.id
+  storage_account_url                   = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net"
+  storage_container_name                = azurerm_storage_container.files.name
+  storage_container_id                  = azurerm_storage_container.files.id
+  storage_account_id                    = azurerm_storage_account.storage.id
+  key_vault_id                          = azurerm_key_vault.kv.id
+  key_vault_url                         = azurerm_key_vault.kv.vault_uri
+  azure_tenant_id                       = var.azure_tenant_id
   azure_client_id_entra                 = var.azure_client_id_entra
   allowed_origins                       = "https://${azurerm_static_web_app.ui.default_host_name}"
   applicationinsights_connection_string = azurerm_application_insights.ai.connection_string
@@ -31,19 +31,19 @@ module "api_prod" {
 module "api_staging" {
   source = "./modules/container_app"
 
-  name                         = "${var.prefix}-api-staging"
-  environment                  = "staging"
-  resource_group_name          = azurerm_resource_group.rg.name
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  acr_login_server             = azurerm_container_registry.acr.login_server
-  acr_id                       = azurerm_container_registry.acr.id
-  storage_account_url          = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net"
-  storage_container_name       = azurerm_storage_container.files_staging.name
-  storage_container_id         = azurerm_storage_container.files_staging.id
-  storage_account_id           = azurerm_storage_account.storage.id
-  key_vault_id                 = azurerm_key_vault.kv.id
-  key_vault_url                = azurerm_key_vault.kv.vault_uri
-  azure_tenant_id              = var.azure_tenant_id
+  name                                  = "${var.prefix}-api-staging"
+  environment                           = "staging"
+  resource_group_name                   = azurerm_resource_group.rg.name
+  container_app_environment_id          = azurerm_container_app_environment.env.id
+  acr_login_server                      = azurerm_container_registry.acr.login_server
+  acr_id                                = azurerm_container_registry.acr.id
+  storage_account_url                   = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net"
+  storage_container_name                = azurerm_storage_container.files_staging.name
+  storage_container_id                  = azurerm_storage_container.files_staging.id
+  storage_account_id                    = azurerm_storage_account.storage.id
+  key_vault_id                          = azurerm_key_vault.kv.id
+  key_vault_url                         = azurerm_key_vault.kv.vault_uri
+  azure_tenant_id                       = var.azure_tenant_id
   azure_client_id_entra                 = var.azure_client_id_entra
   allowed_origins                       = "https://${azurerm_static_web_app.ui.default_host_name}"
   applicationinsights_connection_string = azurerm_application_insights.ai.connection_string
